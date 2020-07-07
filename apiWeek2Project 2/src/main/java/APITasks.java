bf4d97ce5d1eb26da39efa2eca17d2f2732781f2
Silond <silond@gmail.com>
2020-07-07 12:46:38 -0500

    task get AllGoalKeepers for team/77

--- apiWeek2Project 2/src/main/java/APITasks.java
+++ apiWeek2Project 2/src/main/java/APITasks.java
@@ -1,6 +1,18 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Assert;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by nurkulov 12/26/19
@@ -20,8 +32,42 @@ public class APITasks {
      * note: England team id is 66
      * Deserialization type: TypeReference
      */
-    public static List<String> getAllGoalkeepers() throws URISyntaxException, IOException {
-        return null;
+    public static List<String> getAllGoalkeepers() throws URISyntaxException, IOException, NullPointerException {
+
+        HttpClient client = HttpClientBuilder.create().build();
+
+        //  http://api.football-data.org/v2/teams/66
+        URIBuilder uriBuilder = new URIBuilder();
+        uriBuilder.setScheme("http").setHost("api.football-data.org").setPath("v2/teams/66");
+
+        HttpGet get = new HttpGet(uriBuilder.build());
+        get.setHeader("Accept", "application/json");
+        get.setHeader("X-Auth-Token", "72bd7f61c55842bd88ee905ed35f15db");
+
+        HttpResponse response = client.execute(get);
+
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+
+        ObjectMapper objectMapper= new ObjectMapper();
+      Map<String, Object> allGoalKeepers = objectMapper.readValue(response.getEntity().getContent(),
+                new TypeReference<Map<String, Object>>() {
+                });
+
+     List<Map<String, Object>> squad= (List<Map<String, Object>>) allGoalKeepers.get("squad");
+
+        List<String> goalKeepers= new ArrayList<>();
+
+        try {
+            for (int i=0; i<squad.size(); i++){
+                if(squad.get(i).get("position").equals("Goalkeeper")){
+                    System.out.println(squad.get(i).get("name"));
+                    goalKeepers.add(squad.get(i).get("name").toString());
+                }
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return goalKeepers;
     }
 
     /*
@@ -32,6 +78,7 @@ public class APITasks {
     public static List<String> getDefenders() throws URISyntaxException, IOException {
         return null;
     }
+
 
     /*
      * GET names of all midfielders from England team
@@ -66,7 +113,7 @@ public class APITasks {
      * Deserialization type: Pojo
      */
     public static List<String> getSpainCoach() throws URISyntaxException, IOException {
-        return null;
+          return null;
     }
 
     /*
